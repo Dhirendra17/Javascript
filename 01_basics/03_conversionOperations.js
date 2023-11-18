22fffc7bd4ad36472020b9a2da43996d503d2f3c
Dhirendra Patil <dhirendrapatil777@gmail.com>
2023-11-18 14:03:34 +0530

    adds conversion types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+// let score = "77" // 77 is created as 77
+// let score = "77abc" //abc is created as NaN
+// let score = true // true is created as 1
+// let score = false // false is created as 0
+// let score = null // 0
+// let score = undefined // NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
